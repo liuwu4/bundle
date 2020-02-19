264b4d1e1b05f2220302ce43e27bbe0a62d22375
yj <liuwu4@yeah.net>
2020-02-19 17:53:23 +0800

    feat: 增加全局的样式文件

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './pages/App';
+import Index from '@src/layout/index';
+import '@src/utils/global.less';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Index />, document.getElementById('app'));
